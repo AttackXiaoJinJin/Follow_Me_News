1514dbd57c5f1e35161fd2268ea6d1c61777e231
AttackXiaoJinJin <wwwcenjin1314@gmail.com>
2017-07-07 14:21:54 +0800

    文件方式缓存文本

--- app/src/main/java/com/project/chenjin/follow_me_news/until/CacheUntil.java
+++ app/src/main/java/com/project/chenjin/follow_me_news/until/CacheUntil.java
@@ -7,6 +7,15 @@ package com.project.chenjin.follow_me_news.until;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Environment;
+
+import org.xutils.common.util.LogUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 
 public class CacheUntil {
     public static boolean getBoolean(Context context, String key) {
@@ -25,13 +34,91 @@ public class CacheUntil {
     }
     //缓存文本数据
     public static void putString(Context context, String key, String value) {
-        SharedPreferences sp= context.getSharedPreferences("chenjin" , context.MODE_PRIVATE);
-        sp.edit().putString(key, value).commit();
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+
+            try {
+
+                String fileName = MD5Encoder.encode(key);
+                File file = new File(Environment.getExternalStorageDirectory() + "/followmenews" , fileName);
+                LogUtil.e("位置：++++++++++" + Environment.getExternalStorageDirectory());
+
+                File parentFile =  file.getParentFile();//mnt/sdcard/beijingnews
+                if(!parentFile.exists()){
+                    //创建目录
+                    parentFile.mkdirs();
+                }
+
+                if(!file.exists()){
+                    file.createNewFile();
+                }
+                //保存文本数据
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write(value.getBytes());
+                fileOutputStream.close();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                LogUtil.e("文本数据保存失败");
+            }
+        }else{
+            SharedPreferences sp= context.getSharedPreferences("chenjin" , context.MODE_PRIVATE);
+            sp.edit().putString(key, value).commit();
+        }
+
+
+
     }
 
+    //获取缓存文本信息
     public static String getString(Context context, String key) {
-        SharedPreferences sp= context.getSharedPreferences("chenjin" , context.MODE_PRIVATE);
-        //return  sp.getString(key, none);会崩溃
-        return  sp.getString(key, "");
+        String result = "" ;
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            //保存图片在路径xxxxxx下
+            //MD5加密
+            try {
+
+                String fileName = MD5Encoder.encode(key);
+                File file = new File(Environment.getExternalStorageDirectory() + "/followmenews" , fileName);
+                LogUtil.e("位置：++++++++++" + Environment.getExternalStorageDirectory());
+                //如果存在，以流的方式去获取
+                if(file.exists()){
+                    FileInputStream is = new FileInputStream(file);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = is.read(buffer)) != -1 ){
+                        stream.write(buffer, 0 ,length);
+                    }
+
+                    is.close();
+                    stream.close();
+
+                    result = stream.toString();
+
+
+
+
+                }
+
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                LogUtil.e("获取缓存文本失败");
+            }
+        }else {
+            SharedPreferences sp= context.getSharedPreferences("chenjin" , context.MODE_PRIVATE);
+            //return  sp.getString(key, none);会崩溃
+            result = sp.getString(key, "");
+
+
+        }
+
+      return result;
+
+
     }
 }
